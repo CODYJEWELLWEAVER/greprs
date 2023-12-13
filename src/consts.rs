09f8fa46bdffb376662aa41c2b2d8b42b7f9b681
Cody Weaver <Cody.Weaver@colorado.edu>
2023-12-12 21:35:28 -0700

    save progress for rebase

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,4 @@
 // Name of case sensitivity variable. Set with export CASE_INSENSITIVE=(0 or 1)
 pub const CASE_INSENSITIVE_VAR: &str = "CASE_INSENSITIVE";
+// greprs version
 pub const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
