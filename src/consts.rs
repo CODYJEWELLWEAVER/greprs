3ebc279799ba7e0cae03a4683d553b68b489fb7f
Cody Weaver <Cody.Weaver@colorado.edu>
2023-12-12 21:33:51 -0700

    progress

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,3 @@
 // Name of case sensitivity variable. Set with export CASE_INSENSITIVE=(0 or 1)
 pub const CASE_INSENSITIVE_VAR: &str = "CASE_INSENSITIVE";
+pub const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
