aa894f6a9cc19a5f4145a4f33ea090abcc0762fd
Cody Weaver <Cody.Weaver@colorado.edu>
2023-12-18 16:58:27 -0700

    update greprs help output

--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -3,11 +3,7 @@ pub const CASE_INSENSITIVE_VAR: &str = "CASE_INSENSITIVE";
 
 // GREPRS VERSION
 pub const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
-pub const VERSION_PREFIX: &str = "v";
 pub const UNKNOWN_VERSION: &str = "UNKNOWN VERSION";
-
-// HELP INFORMATION
-pub const HELP_INFORMATION_OUTPUT: &str = "TODO:";
 
 // CASE SENSITIVITY ARGUMENTS
 pub const CASE_INSENSITIVE_ARG_0: &str = "-ignore-case";
@@ -21,3 +17,18 @@ pub const GREPRS_VERSION_OPTION: &str = "version";
 // GREPRS ERROR MSGS
 pub const ERROR_MSG_USAGE_HINT: &'static str = "Not enough arguments: Run 'greprs help' for usage help...";
 
+// HELP INFORMATION OUTPUT
+///////////////////////////
+// Output of '$greprs help'
+pub const HELP_INFORMATION_OUTPUT: &str
+= "Welcome to greprs:
+*******************************************************************************
+Usage: 
+  -Basic: $greprs <query> <path/to/file>
+  -Basic w/ options: $greprs <query> <path/to/file> [<option>, ...]
+  -Alt: $greprs <query> in <path/to/file>
+  -Alt w/ options: $greprs <query> in <path/to/file> with [<option>, ...]
+*******************************************************************************
+Help:
+  -Documentation: $greprs help
+  -Version Info: $greprs version";
